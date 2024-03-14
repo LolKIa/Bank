72b4b945e12b456a874344b0077cbc53aeba34b3
LolKia <remeyztoor@gmail.com>
2024-03-14 15:16:18 +0900

    Initial commit

---
+++ app/src/main/java/com/example/bank/MainActivity2.java
@@ -0,0 +1,22 @@
+package com.example.bank;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.content.Context;
+import android.graphics.Camera;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity2 extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+    }
+
+}
