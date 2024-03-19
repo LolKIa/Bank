006bcc6c0aadf670023d3cd55b86bc0cd0c9a618
LolKia <remeyztoor@gmail.com>
2024-03-19 09:59:55 +0900

    sdf

--- app/src/main/java/com/example/bank/MainActivity.java
+++ app/src/main/java/com/example/bank/MainActivity.java
@@ -1,21 +1,76 @@
 package com.example.bank;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputFilter;
 import android.view.View;
+import android.view.animation.LinearInterpolator;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private Button btnV;
+    Context context;
+    ConstraintLayout constraintLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnV = (Button)findViewById(R.id.button6);
+        constraintLayout = (ConstraintLayout)findViewById(R.id.ConstraintLayout);
+        context = MainActivity.this;
+
+        btnV.setOnClickListener(this);
     }
 
-    public void onClick(View v) {
+    public void onClick1(View v) {
         Intent intent = new Intent(MainActivity.this, MainActivity2.class);
         startActivity(intent);
+    }
+
+    public void onClick2(View v) {
+        Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onClick(View v) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(R.string.message);
+        builder.setMessage(R.string.tint);
+
+        LinearLayout layout = new LinearLayout(this);
+        layout.setOrientation(LinearLayout.VERTICAL);
+
+        final EditText editText = new EditText(this);
+        editText.setText(R.string.login);
+        layout.addView(editText);
+
+        final EditText editText2 = new EditText(this);
+        editText2.setHint(R.string.password);
+        layout.addView(editText2);
+
+
+        builder.setView(layout);
+
+
+        builder.setPositiveButton(R.string.otm, null);
+        builder.setNegativeButton(R.string.ok, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(intent);
+            }
+        });
+        builder.show();
     }
 }
